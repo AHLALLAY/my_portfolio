a3aebd624f2855f3a93bb9ae5cf75111267fe33f
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-06 23:44:24 +0100

    refact: Renommer le model 'Project'

--- Code/Backend/app/Models/Project.php
+++ Code/Backend/app/Models/Project.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Projet extends Model
+class Project extends Model
 {
     protected $fillable = ['title', 'description', 'start_date', 'end_date', 'homepage_url'];
 }
