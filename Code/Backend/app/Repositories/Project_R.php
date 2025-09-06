59e6c0b9513ffcb15914d58a6c24a5c07e1ab413
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-06 23:52:13 +0100

    feat: Implementer 'getProjects'

--- Code/Backend/app/Repositories/Project_R.php
+++ Code/Backend/app/Repositories/Project_R.php
@@ -2,11 +2,18 @@
 
 namespace App\Repositories;
 use App\Interfaces\Project_I;
+use App\Models\Project;
 
 class Project_R implements Project_I
 {
     // CRUD
-    public function getProjects(){}
+    public function getProjects(){
+        try{
+            return Project::all();
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
     public function getProject($id){}
     public function createProject($project){}
     public function editeProject($projectId){}
