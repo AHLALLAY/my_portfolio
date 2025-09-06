79a8136c33e41c1c71179e6407469b12bd3f3340
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-06 23:51:44 +0100

    feat: Ajouter route du 'Project_C'

--- Code/Backend/routes/api.php
+++ Code/Backend/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\Project_C;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::controller(Project_C::class)->group(function(){
+    // CRUD
+    Route::get('/projects', 'getProjects');
+    Route::get('/project/{id}', 'getProject');
+    Route::post('/projects', 'createProject');
+    Route::put('/project/{id}', 'editeProject');
+    Route::delete('/project/{id}', 'deleteProjects');
+    
+    // STATS
+    Route::get('/projects/total', 'projectTotal');
+    Route::get('/projects/archive/total', 'projectArchive');
+});
