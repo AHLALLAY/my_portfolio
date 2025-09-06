433a0fd081cc899f8a736ca919599ef9054e6dbb
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-06 17:14:01 +0100

    feat: Liaison les repositories avec les interfaces

--- Code/Backend/app/Providers/AppServiceProvider.php
+++ Code/Backend/app/Providers/AppServiceProvider.php
@@ -2,23 +2,20 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Project_I;
+use App\Interfaces\Skill_I;
+use App\Repositories\Project_R;
+use App\Repositories\Skill_R;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(Project_I::class, Project_R::class);
+        $this->app->bind(Skill_I::class, Skill_R::class);
+        
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+    public function boot(): void {}
 }
