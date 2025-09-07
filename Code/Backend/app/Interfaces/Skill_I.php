a133ca2bc01c5c57f4cf6e2024abb606c7c18cb0
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-07 19:56:29 +0100

    feat: Implèmenter tous les mèthodes de 'Skill_R'

--- Code/Backend/app/Interfaces/Skill_I.php
+++ Code/Backend/app/Interfaces/Skill_I.php
@@ -8,7 +8,7 @@ interface Skill_I
     public function getSkills();
     public function getSkill($skillId);
     public function createSkill($skill);
-    public function editeSkill($skillId);
+    public function editeSkill($skillId, $new_skill);
     public function deleteSkill($skillId);
 
     // STATS
--- Code/Backend/app/Repositories/Skill_R.php
+++ Code/Backend/app/Repositories/Skill_R.php
@@ -2,16 +2,50 @@
 
 namespace App\Repositories;
 use App\Interfaces\Skill_I;
+use App\Models\Skill;
 
 class Skill_R implements Skill_I
 {
     // CRUD
-    public function getSkills(){}
-    public function getSkill($skillId){}
-    public function createSkill($skill){}
-    public function editeSkill($skillId){}
-    public function deleteSkill($skillId){}
+    public function getSkills(){
+        try{
+            return Skill::all();
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
+    public function getSkill($skillId){
+        try{
+            return Skill::findOrFail($skillId);
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
+    public function createSkill($skill){
+        try{
+            return Skill::create($skill);
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
+    public function editeSkill($skillId, $new_skill){}
+    public function deleteSkill($skillId){
+        try{
+            $skill = Skill::findOrFail($skillId);
+            if($skill){
+                return $skill->delete();
+            }
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
 
     // STATS
-    public function skillTotal(){}
+    public function skillTotal(){
+        try{
+            return Skill::all()->count();
+        }catch(\Exception $e){
+            throw $e;
+        }
+    }
 }
