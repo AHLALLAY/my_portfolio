c92051892f19410ee59ea0780f4ff07ea53268ec
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-09-07 20:46:51 +0100

    feat: Ajouter les relations dans les models

--- Code/Backend/app/Models/Project.php
+++ Code/Backend/app/Models/Project.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['title', 'description', 'start_date', 'end_date', 'homepage_url'];
+
+    public function skills(){ return $this->belongsToMany(Skill::class, 'project_skills'); }
 }
--- Code/Backend/app/Models/Skill.php
+++ Code/Backend/app/Models/Skill.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     protected $fillable = ['name', 'type', 'start_date', 'end_date', 'is_certified', 'certification_date'];
+
+    public function projects(){ return $this->belongsToMany(Project::class, 'project_skills'); }
 }
